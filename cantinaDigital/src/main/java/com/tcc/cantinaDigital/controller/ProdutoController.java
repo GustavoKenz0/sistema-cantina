23d7e5f91c67165148b52b457d961b0ce835cd97
vitorlescola <158179013+vitorlescola@users.noreply.github.com>
2024-10-27 16:22:56 -0300

    Add files via upload

--- cantinaDigital/src/main/java/com/tcc/cantinaDigital/controller/ProdutoController.java
+++ cantinaDigital/src/main/java/com/tcc/cantinaDigital/controller/ProdutoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tcc.cantinaDigital.model.Produto;
+import com.tcc.cantinaDigital.model.Tipo;
 import com.tcc.cantinaDigital.repository.ProdutoRepository;
 
 @Controller
@@ -29,32 +30,37 @@ public class ProdutoController {
 	
 	@GetMapping("/listaLanches")
 	public String listaLanches(Model modelo) {
-		modelo.addAttribute("produtos",produtoRepository.findAll());
-		return "ListaLanches";
+	    modelo.addAttribute("produtos", produtoRepository.findByTipo(Tipo.Salgado));
+	    return "ListaLanches";
 	}
 	
 	@GetMapping("/LanchesAdm")
-	public String LanchesAdm() {
+	public String LanchesAdm(Model modelo) {
+		modelo.addAttribute("produtos", produtoRepository.findByTipo(Tipo.Salgado));
 		return "LanchesAdm";
 	}
 	
 	@GetMapping("/listaBebidas")
-	public String listarBebidas() {
+	public String listarBebidas(Model modelo) {
+		modelo.addAttribute("produtos", produtoRepository.findByTipo(Tipo.Bebida));
 		return "ListarBebidas";
 	}
 	
 	@GetMapping("/bebidasAdm")
-	public String bebidasAdm() {
+	public String bebidasAdm(Model modelo) {
+		modelo.addAttribute("produtos", produtoRepository.findByTipo(Tipo.Bebida));
 		return "BebidasAdm";
 	}
 	
 	@GetMapping("/listaDoces")
-	public String listarDoces() {
+	public String listarDoces(Model modelo) {
+		modelo.addAttribute("produtos", produtoRepository.findByTipo(Tipo.Doce));
 		return "ListarDoces";
 	}
 	
 	@GetMapping("/docesAdm")
-	public String docesAdm() {
+	public String docesAdm(Model modelo) {
+		modelo.addAttribute("produtos", produtoRepository.findByTipo(Tipo.Doce));
 		return "docesAdm";
 	}
 	
